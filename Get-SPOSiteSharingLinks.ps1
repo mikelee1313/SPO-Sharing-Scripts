408571d39ef3a38cfb4992e0ea279a68a8469ecf
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2024-06-13 11:25:28 -0400

    Update Get-SPOSiteSharingLinks.ps1

--- Get-SPOSiteSharingLinks.ps1
+++ Get-SPOSiteSharingLinks.ps1
@@ -1,6 +1,8 @@
 <#   
 .SYNOPSIS
-    Get-SPOSiteSharingLinks.ps1 -  Loops through all specified sites and exports any sites that contain Sharing Links, to help identify potential internal oversharing.
+      Get-SPOSiteSharingLinks.ps1 - Loops through all specified sites and exports all SharePoint level groups and users for each site as well as user
+      information like permission level and IB segments if applicable. The Script will also show any sites that are opened to everyone using the 
+      “spo-grid-all-users” permission level and any sites with Company Wide Sharing Links.
 
 .PARAMETER Tenant
     Specifies the name of the tenant to connect to.
@@ -16,7 +18,7 @@
 
 .NOTES
     Authors: Mike Lee, Kiran Bellala, Brian Mokaya
-    Date: 5/22/2024
+    Date: 6/13/2024
     Disclaimer: The sample scripts are provided AS IS without warranty of any kind. 
     Microsoft further disclaims all implied warranties including, without limitation, 
     any implied warranties of merchantability or of fitness for a particular purpose. 
@@ -27,6 +29,7 @@
     loss of business information, or other pecuniary loss) arising out of the use of or inability 
     to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
 #>
+
 
 #Configurable Settings
 $t = 'contoso' # < - Your Tenant Name Here
@@ -43,8 +46,8 @@ $date = Get-Date -Format yyyy-MM-dd_HH-mm-ss
 
 #Input / Output and Log Files
 $inputfile = 'C:\temp\sitelist.csv'
-$outputfile = "$env:TEMP\" + 'SiteInfo_' + $date + '_' + "output.csv"
-$log = "$env:TEMP\" + 'SiteInfo_' + $date + '_' + "logfile.log"
+$outputfile = "$env:TEMP\" + 'SPSite_SharingLinks_' + $date + '_' + "output.csv"
+$log = "$env:TEMP\" + 'SPSite_SharingLinks_' + $date + '_' + "logfile.log"
 
 #Connect to Services
 Connect-SPOService -Url ('https://' + $t + '-admin.sharepoint.com')
@@ -81,8 +84,6 @@ Function Write-LogEntry {
 #use simple imput file
 $sites = Import-csv -path $inputfile -Header 'URL'
 
-
-
 #Add account as admin and export groups
 foreach ($site in $sites) {
 
@@ -114,71 +115,98 @@ foreach ($site in $sites) {
     $siteprops = get-sposite -Identity $site.url | Select-Object URL, Owner, InformationBarriersMode, InformationSegment, GroupId, RelatedGroupId, IsHubSite, Template, SiteDefinedSharingCapability, SharingCapability, DisableCompanyWideSharingLinks, IsTeamsConnected, IsTeamsChannelConnected, TeamsChannelType
 
     if ($siteprops.GroupId.Guid -ne '00000000-0000-0000-0000-000000000000') {
-        $groupmembers = Get-UnifiedGroupLinks -Identity  $siteprops.GroupId -LinkType Members
         $groupowners = Get-UnifiedGroupLinks -Identity  $siteprops.GroupId -LinkType Owners
+        $groupmembers = Get-UnifiedGroupLinks -Identity  $siteprops.GroupId -LinkType Members
         $AADGroups = Get-UnifiedGroup -Identity $siteprops.GroupId | Select-Object Guid, DisplayName, Alias, AccessType, WhenCreated
     }
  
-    Write-host "Information Barrier Mode:" $siteprops.InformationBarriersMode -ForegroundColor White
+    #Write-host "Information Barrier Mode:" $siteprops.InformationBarriersMode -ForegroundColor White
     Write-LogEntry -LogName:$Log -LogEntryText "Information Barrier Mode: $($siteprops.InformationBarriersMode)"
  
-    Write-host "Information Barrier Segment:" $siteprops.InformationSegment -ForegroundColor White
-    Write-LogEntry -LogName:$Log -LogEntryText  "Information Barrier Segment:" $($siteprops.InformationSegment)
+    #Write-host "Information Barrier Segment:" $siteprops.InformationSegment -ForegroundColor White
+    Write-LogEntry -LogName:$Log -LogEntryText  "Information Barrier Segment: $($siteprops.InformationSegment)"
 
     #Get All Groups of a site collection
     $Groups = Get-SPOSiteGroup -Site $site.Url
 
-    Write-host "Total Number of Groups Found:" $Groups.Count -ForegroundColor White
+    #Write-host "Total Number of Groups Found:" $Groups.Count -ForegroundColor White
     Write-LogEntry -LogName:$Log -LogEntryText "Total Number of Groups Found: $($Groups.Count)"
 
     ForEach ($Group in $Groups) {
 
         #If statement is to only collect sites that contain sharing links 
         if ($group.Title -like 'SharingLinks*') {
+    
+            #Write-Host "Group Title: $($Group.Title)" -ForegroundColor Yellow
+            Write-LogEntry -LogName:$Log -LogEntryText "Group Title: '$($Group.Title)'"
 
-            Write-Host "Group Title: $($Group.Title)" -ForegroundColor Yellow
-            Write-LogEntry -LogName:$Log -LogEntryText "Group Title: $($Group.Title)"
+            #Write-Host "Group Roles: $($Group.Roles) " -ForegroundColor Red
+            Write-LogEntry -LogName:$Log -LogEntryText "Group Roles: '$($Group.Roles)'" 
 
-            Write-Host "Group Roles: $($Group.Roles) " -ForegroundColor Red
-            Write-LogEntry -LogName:$Log -LogEntryText "Group Roles: $($Group.Roles)" 
-
-            Write-Host "Users in Group: '$($Group.Users)'" -ForegroundColor Cyan
+            #Write-Host "Users in Group: '$($Group.Users)'" -ForegroundColor Cyan
             Write-LogEntry -LogName:$Log -LogEntryText "Users in Group: '$($Group.Users)'"
 
             Write-Host ""
             Write-LogEntry -LogName:$Log -LogEntryText ""
 
-            #Collecting Export Properties for CSV File
-            $ExportItem = New-Object PSObject
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "URL" -value ($($siteprops.url))
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Owner" -value ($($siteprops.Owner))  
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "IB Mode" -value ($($siteprops.InformationBarriersMode) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "IB Segment" -value ($($siteprops.InformationSegment) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Group ID" -value ($($siteprops.GroupId) -join ',')    
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "RelatedGroupId" -value ($($siteprops.RelatedGroupId) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "IsHubSite" -value ($($siteprops.IsHubSite) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Template" -value ($($siteprops.Template) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "SiteDefinedSharingCapability" -value ($($siteprops.SiteDefinedSharingCapability) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "SharingCapability" -value ($($siteprops.SharingCapability) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "DisableCompanyWideSharingLinks" -value ($($siteprops.DisableCompanyWideSharingLinks) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "IsTeamsConnected" -value ($($siteprops.IsTeamsConnected) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "IsTeamsChannelConnected" -value ($($siteprops.IsTeamsChannelConnected) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "TeamsChannelType" -value ($($siteprops.TeamsChannelType) -join ',')
-  
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "SPGroup Title" -value ($($Group.Title))
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "SPGroup Users" -value ($($Group.Users) -join ',')
+            # This script block iterates over each user in a group.
+            # For each user, it attempts to retrieve the user's display name, primary SMTP address, and information barrier segments using the Get-Recipient cmdlet.
+            # The retrieved information is then logged using a custom Write-LogEntry function.
+            # If the script is unable to retrieve the information for a user, it prints an error message to the console and logs the error.  
+            ForEach ($GroupUser in $Group.Users) {
+       
+                $guser = @()
+                # Attempt to retrieve user information
+                try {
+                    $guser = Get-Recipient -Identity $GroupUser | Select-Object DisplayName, PrimarySmtpAddress, InformationBarrierSegments
+                    Write-LogEntry -LogName:$Log "User DisplayName is '$($guser.DisplayName)'"
+                    Write-LogEntry -LogName:$Log "User E-Mail Address is  '$($guser.PrimarySmtpAddress)'"
+                    Write-LogEntry -LogName:$Log "User InfoSegment is '$($guser.InformationBarrierSegments)'"
 
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group Displayname" -value ($($AADGroups.DisplayName))
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group Alias" -value ($($AADGroups.Alias) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group AccessType" -value ($($AADGroups.AccessType) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group ManagedBy" -value ($($AADGroups.ManagedBy) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group WhenCreated" -value ($($AADGroups.WhenCreated) -join ',')
-        
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group Owners" -value ($($groupowners.DisplayName) -join ',')
-            $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group Members" -value ($($groupmembers.DisplayName) -join ',')
-            $output += $ExportItem
+                }
+                # Print and log error message if unable to retrieve user information
+                catch {
+                    Write-Host "Unable to retrieve information for user: $GroupUser" -ForegroundColor Red
+                    Write-LogEntry -LogName:$Log -LogEntryText "Unable to retrieve information for user: $GroupUser"
+                }
+            
+                #define spo-grid-all-users
+                $allusers = $Group.Users -like '*spo-grid-all-users*'
+
+                #Collecting Export Properties for CSV File
+                $ExportItem = New-Object PSObject
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "URL" -value ($($siteprops.url))
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "Owner" -value ($($siteprops.Owner))  
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "IB Mode" -value ($($siteprops.InformationBarriersMode) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "IB Segment" -value ($($siteprops.InformationSegment) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "Group ID" -value ($($siteprops.GroupId) -join ',')    
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "RelatedGroupId" -value ($($siteprops.RelatedGroupId) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "IsHubSite" -value ($($siteprops.IsHubSite) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "Template" -value ($($siteprops.Template) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "SiteDefinedSharingCapability" -value ($($siteprops.SiteDefinedSharingCapability) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "SharingCapability" -value ($($siteprops.SharingCapability) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "DisableCompanyWideSharingLinks" -value ($($siteprops.DisableCompanyWideSharingLinks) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "IsTeamsConnected" -value ($($siteprops.IsTeamsConnected) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "IsTeamsChannelConnected" -value ($($siteprops.IsTeamsChannelConnected) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "TeamsChannelType" -value ($($siteprops.TeamsChannelType) -join ',')
+            
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "SPGroup Title" -value ($($Group.Title))
+
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "SP User Name" -value ($($guser.DisplayName))
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "SP User E-mail" -value ($($guser.PrimarySmtpAddress) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "SP Group User InfoSegment" -value ($($guser.InformationBarrierSegments) -join ',')
+
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group Displayname" -value ($($AADGroups.DisplayName))
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group Alias" -value ($($AADGroups.Alias) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group AccessType" -value ($($AADGroups.AccessType) -join ',')
+                $ExportItem  | Add-Member -MemberType NoteProperty -name "Entra Group WhenCreated" -value ($($AADGroups.WhenCreated) -join ',')
+
+                $output += $ExportItem
+            }
         }
+
     }
+
     #Removing Admin Account as a Site Collection Admin
     Try {
        
@@ -193,11 +221,10 @@ foreach ($site in $sites) {
     }
     Write-Host ""
     Write-LogEntry -LogName:$Log -LogEntryText ""
+
 }
 
 #Export the data to CSV
-$output | Export-Csv $outputfile -NoTypeInformation -Append
-
-
+$output | Export-Csv $outputfile -NoTypeInformation -Append   
 Write-Host " === === === === === Completed! === === === === === === == "
 Write-Host "Collection output file $outputfile was saved" 
