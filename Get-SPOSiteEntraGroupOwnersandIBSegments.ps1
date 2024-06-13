6fcdd2d325eb00ee940185e00f423bfc5de56046
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2024-06-13 10:35:15 -0400

    Update and rename Get-SPOSiteOwnersandSegments.ps1 to Get-SPOSiteEntraGroupOwnersandIBSegments.ps1

--- Get-SPOSiteEntraGroupOwnersandIBSegments.ps1
+++ Get-SPOSiteEntraGroupOwnersandIBSegments.ps1
@@ -1,6 +1,6 @@
 <#   
 .SYNOPSIS
-    Get-SPOSiteOwnersandSegments.ps1 - Loops through all sites and exports all groups and users for each site.
+    Get-SPOSiteEntraGroupOwnersandIBSegments.ps1 - Loops through all sites and exports all groups and users for each site.
 
 .DESCRIPTION
     This script connects to SharePoint Online and Exchange Online services to retrieve information about sites and their associated groups and users. It exports the collected data to a CSV file.
