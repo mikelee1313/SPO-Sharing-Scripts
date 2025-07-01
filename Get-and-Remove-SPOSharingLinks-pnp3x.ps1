9b102b5370312af3087657812c228e8684c1e36c
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2025-07-01 15:35:19 -0400

    Update Get-and-Remove-SPOSharingLinks-pnp3x.ps1

--- Get-and-Remove-SPOSharingLinks-pnp3x.ps1
+++ Get-and-Remove-SPOSharingLinks-pnp3x.ps1
@@ -77,11 +77,11 @@
 
 .EXAMPLE
     # Process sites from a simple CSV file with URLs and convert Organization links to direct permissions
-    $tenantname = "contoso"
+    $tenantname = "m365x61250205"
     $appID = "12345678-1234-1234-1234-1234567890ab"
     $thumbprint = "1234567890ABCDEF1234567890ABCDEF12345678"
     $tenant = "12345678-1234-1234-1234-1234567890ab"
-    $inputfile = "C:\temp\sitelist.csv"
+    $inputfile = $null
     $convertOrganizationLinks = $false
     # Note: $cleanupCorruptedSharingGroups will automatically be set to $true in remediation mode
     .\Get-and-Remove-SPOSharingLinks-pnp3x.ps1
