a6304511d1e5fe330d879344cb0fb06bc0f5a2a5
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2025-11-13 18:08:26 -0500

    Remove Andrew Thompson as author from script

--- Get-SPSitesOwners.ps1
+++ Get-SPSitesOwners.ps1
@@ -24,7 +24,7 @@
 
 .NOTES
     File Name      : Get-SPSitesOwners.ps1
-    Author         : Mike Lee / Andrew Thompson
+    Author         : Mike Lee
     Prerequisite   : PnP.PowerShell module installed
     Date           : 11/13/25     
     Version        : 3.0
